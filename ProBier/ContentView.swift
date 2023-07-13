ba8ee6990e663c56d3a0e69ca5e4b474941ecb63
Hans-Christian Wollert <hwollert@icloud.com>
2023-07-13 13:43:36 +0200

    Initiate project

--- ProBier/ContentView.swift
+++ ProBier/ContentView.swift
@@ -83,6 +83,8 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
